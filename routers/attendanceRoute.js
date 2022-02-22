54b5604ad6c635f21daa2c090fd7c10ee5322905
fanotastic <fawwazputrou@gmail.com>
2022-02-22 14:02:39 +0700

    AA-10: fix bug attendanceRoute

--- routers/attendanceRoute.js
+++ routers/attendanceRoute.js
@@ -5,5 +5,8 @@ const router = require ('express').Router()
 
 router.get('/', attendanceController.getData);
 router.get('/:id', attendanceController.studentAttendance)
+router.get('/session', attendanceController.getSessionStudent);
+router.post('/checkin', attendanceController.checkIn);
+router.patch('/checkout/:idattendance', attendanceController.checkOut);
 
 module.exports = router
--- routers/attendanceRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const {attendanceController} = require('../controllers');
-
-router.get('/', attendanceController.getSessionStudent);
-router.post('/checkin', attendanceController.checkIn);
-router.patch('/checkout/:idattendance', attendanceController.checkOut);
-
-module.exports = router
