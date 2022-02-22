96cc1be2ea93e8e0c875ee32181bb7131177a116
alifjunjunan <junjunanalif@gmail.com>
2022-02-22 13:24:20 +0700

    AA-8-02: Update fungsi dan proteksi Checkin & Checkout

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -7,13 +7,34 @@ module.exports = {
         try {
 
             //get data session student yang sedang login
-            let getStudent = await dbQuery(`select u.*, s.session, s.timein, s.timeout FROM users as u JOIN sessions as s ON u.idsession = s.idsession WHERE iduser=1;`);
+            let getStudent = await dbQuery(`select u.*, s.session, s.timein, s.timeout FROM users as u JOIN session as s ON u.idsession = s.idsession WHERE iduser=${db.escape(req.dataStudent.iduser)};`);
 
+            // console.log('isi getStudent', getStudent)
             res.status(200).send({
                 message: 'success get data student',
                 success: true,
                 dataSessionStudent: getStudent
             })
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({
+                message: 'Failed',
+                success: false
+            })
+        }
+    },
+    getAttendanceStudent : async (req,res) => {
+
+        try {
+            let getAttendance = await dbQuery(`SELECT * FROM attendance WHERE iduser=${db.escape(req.dataStudent.iduser)} AND date=${db.escape(req.params.date)}`)
+            
+            // console.log('isi getAttendance =>', getAttendance)
+            res.status(200).send({
+                message: 'success get data attendance',
+                success: true,
+                dataAttendance: getAttendance
+            })
+
         } catch (error) {
             console.log(error)
             res.status(500).send({
@@ -29,13 +50,13 @@ module.exports = {
             let { date, checkin } = req.body
 
             //get data session student yang sedang login
-            let getStudent = await dbQuery(`select u.*, s.session, s.timein, s.timeout FROM users as u JOIN sessions as s ON u.idsession = s.idsession WHERE iduser=1;`);
+            let getStudent = await dbQuery(`select u.*, s.session, s.timein, s.timeout FROM users as u JOIN session as s ON u.idsession = s.idsession WHERE iduser=${db.escape(req.dataStudent.iduser)};`);
 
             //cek status berdasarkan waktu checkin dan timein
             let idstatus = checkin <= getStudent[0].timein ? 3 : 4;
 
             //insert data attendance
-            let sqlInsert = `INSERT INTO attendances VALUES (null,${db.escape(getStudent[0].iduser)}, ${db.escape(idstatus)},
+            let sqlInsert = `INSERT INTO attendance VALUES (null,${db.escape(req.dataStudent.iduser)}, ${db.escape(idstatus)},
             ${db.escape(date)}, ${db.escape(checkin)},null);`
 
             let InsertAttendance = await dbQuery(sqlInsert)
@@ -64,7 +85,7 @@ module.exports = {
             let { checkout } = req.body
 
             //update kolom checkout
-            await dbQuery(`UPDATE attendances set checkout=${db.escape(checkout)} WHERE idattendance=${req.params.idattendance}`)
+            await dbQuery(`UPDATE attendance set check_out=${db.escape(checkout)} WHERE idattendance=${req.params.idattendance}`)
 
             res.status(200).send({
                 message: 'success checkout',
--- routers/attendanceRoutes.js
+++ routers/attendanceRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const { readToken } = require('../config/jwt');
 const {attendanceController} = require('../controllers');
 
-router.get('/', attendanceController.getSessionStudent);
-router.post('/checkin', attendanceController.checkIn);
-router.patch('/checkout/:idattendance', attendanceController.checkOut);
+router.get('/session', readToken ,attendanceController.getSessionStudent);
+router.get('/:date', readToken ,attendanceController.getAttendanceStudent);
+router.post('/checkin', readToken, attendanceController.checkIn);
+router.patch('/checkout/:idattendance', readToken, attendanceController.checkOut);
 
 module.exports = router
